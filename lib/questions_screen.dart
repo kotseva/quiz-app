ac8450efe6dd51c5b7e80891dc4bd6f702b60729
Angela Koceva <angelakoceva1@gmail.com>
2024-12-23 21:26:30 +0100

    question changes once the button is pressed

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,10 +12,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
-
+    final currentQuestion = questions[currentQuestionIndex];
+    
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -34,7 +41,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
             const SizedBox(height: 20),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(answerText: answer, onTap: () {});
+              return AnswerButton(
+                answerText: answer,
+                onTap: answerQuestion,
+              );
             })
           ],
         ),
